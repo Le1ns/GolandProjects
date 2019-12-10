37bc7b577a8fe3fc3027c88ca8e63b735982c11b
Le1ns <Lagutka_98@mail.ru>
2019-12-10 17:37:16 +0400

    3 done 4 shablon

--- exercises/slice2.go
+++ exercises/slice2.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-
 )
 
 func main() {
-	p := []int{1,2,3,4,5,6}
-	p = append(p,5)
+	p := []int{1, 2, 3, 4, 5, 6}
+	p = append(p, 5)
 	fmt.Println(p)
 }
---
+++ exercises/slice4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	i := 2
+
+	// Remove the element at index i from a.
+	copy(a[i:], a[i+1:]) // Shift a[i+1:] left one index.
+	a[len(a)-1] = 0      // Erase last element (write zero value).
+	a = a[:len(a)-1]     // Truncate slice.
+
+	fmt.Println(a) // [A B D E]
+
+}
