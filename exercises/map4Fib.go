acdedb1a25e5a4d1b0eefcecb46702458d8611fd
Le1ns <Lagutka_98@mail.ru>
2019-12-16 18:54:35 +0400

    maps

--- exercises/map4Fib.go
+++ exercises/map4Fib.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	newFib := memoize(fib)
+	for i := 1; i < 42; i++ {
+		fmt.Println(newFib(i))
+	}
+}
+
+func memoize(f func(int) int) func(int) int {
+	cache := make(map[int]int)
+	return func(n int) int {
+		if cache[n] == 0 {
+			cache[n] = f(n)
+			fmt.Println("----", cache[n])
+		}
+		return cache[n]
+	}
+}
+
+func fib(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
